8f3d4a81277f3bb73afb155ef8dd2fc6031b4ae4
Porping <porsteam@hotmail.com>
2023-11-05 22:18:41 +0700

    🦾 Reinforcement ma seed 🦾

--- components/single-file-uploader.tsx
+++ components/single-file-uploader.tsx
@@ -65,7 +65,7 @@ const MultipleFileUploader = () => {
       const buffer = await file.arrayBuffer();
       const workBook = xlsx.read(buffer, { type: "buffer" });
 
-      const data = workBook.SheetNames.map((sheetName) => {
+      workBook.SheetNames.map((sheetName) => {
         const sheet = workBook.Sheets[sheetName];
         const eligibles = xlsx.utils.sheet_to_json(sheet, {
           range: 1,
@@ -89,83 +89,98 @@ const MultipleFileUploader = () => {
   };
 
   const exportFile = () => {
+    type ExtendedEligible = EligibleSpreadsheetRow & { year: string }
+
+    type ExtendedEligibleWithPartialyApplicant = ExtendedEligible & Partial<ApplicantSpreadsheetRow>
+    type ExtendedEligibleWithApplicant = ExtendedEligible & ApplicantSpreadsheetRow
+
+    const isExtendedEligibleWithApplicant = (o: ExtendedEligibleWithPartialyApplicant): o is ExtendedEligibleWithApplicant => {
+      return o.hasOwnProperty("ประเภทการเข้า") && o.hasOwnProperty("GPAX")
+    }
+
+    const extendedEligibleWithApplicantToPyaoResult = (e: ExtendedEligibleWithApplicant): PyaoResultSpreadsheetRow => {
+      return (
+        {
+          รหัสนักศึกษา: e["รหัสนักศึกษา"],
+          "ชื่อ-สกุล": e["ชื่อ - สกุล"],
+          ประเภทการเข้า: e["ประเภทการเข้า"],
+          โรงเรียน: e["ชื่อสถานศึกษา"] ?? "",
+          จังหวัด: e["จังหวัดสถานศึกษา"] ?? "",
+          GPAX: e["GPAX"] ?? "",
+          "GPA Math": e["รายการคะแนนกลุ่มสาระวิชา_คณิตศาสตร์"] ?? "",
+          "GPA Science": e["รายการคะแนนกลุ่มสาระวิชา_วิทยาศาสตร์"] ?? "",
+          "GPA English": e["รายการคะแนนกลุ่มสาระวิชา_ภาษาต่างประเทศ"] ?? "",
+          "GPAX 1": e[`1/${Number(e["year"])}`] || "",
+          "GPAX 2": e[`2/${Number(e["year"])}`] || "",
+          "GPAX 3": e[`1/${Number(e["year"]) + 1}`] || "",
+          "GPAX 4": e[`2/${Number(e["year"]) + 1}`] || "",
+          "GPAX 5": e[`1/${Number(e["year"]) + 2}`] || "",
+          "GPAX 6": e[`2/${Number(e["year"]) + 2}`] || "",
+          "GPAX 7": e[`1/${Number(e["year"]) + 3}`] || "",
+          "GPAX 8": e[`2/${Number(e["year"]) + 3}`] || "",
+          จำนวนหน่วยกิตรวม: "0",
+          หมายเหตุ: e["หมายเหตุ"],
+        }
+      )
+    }
+
     const workBook = xlsx.utils.book_new();
 
     const applicantByName = new Map<string, ApplicantSpreadsheetRow>();
-    const eligibleByName = new Map<
-      string,
-      (EligibleSpreadsheetRow & { year: string }) & ApplicantSpreadsheetRow
-    >();
-
-    applicants.forEach((applicant) => {
-      const name =
-        applicant["คำนำหน้านาม(ไทย)"] +
-        applicant["ชื่อ(ไทย)"] +
-        " " +
-        applicant["นามสกุล(ไทย)"];
+    const extendedEligibleWithPartialyApplicantByName = new Map<string, ExtendedEligibleWithPartialyApplicant>();
+    
+    for (const applicant of applicants) {
+      let name
+      if (applicant["สัญชาติ"] == "ไทย") {
+          name = applicant["คำนำหน้านาม(ไทย)"] + applicant["ชื่อ(ไทย)"] + " " + applicant["นามสกุล(ไทย)"];
+      } else {
+          name = applicant["คำนำหน้านาม(อังกฤษ)"] + applicant["ชื่อ(อังกฤษ)"] + " " + applicant["นามสกุล(อังกฤษ)"];
+      }
 
       applicantByName.set(name, applicant);
-    });
+    }
 
-    eligibles.forEach((eligible) => {
-      const year = eligible["รหัสนักศึกษา"].substring(0, 2);
+    for (const eligible of eligibles) {
       const applicant = applicantByName.get(eligible["ชื่อ - สกุล"])
-
       if (!applicant) {
         const text = `${eligible["ชื่อ - สกุล"]} ${eligible["รหัสนักศึกษา"]} is not merged"`
         console.log(text)
         setLogs((prev) => prev + 1)
       }
 
-      const value = {
-        ...applicant!,
+      const year = eligible["รหัสนักศึกษา"].substring(0, 2);
+
+      const value: ExtendedEligibleWithPartialyApplicant = {
+        ...applicant,
         ...{
           ...eligible,
           year,
         },
       };
-      eligibleByName.set(eligible["ชื่อ - สกุล"], value);
-    });
+  
+      extendedEligibleWithPartialyApplicantByName.set(eligible["ชื่อ - สกุล"], value);
+    }
 
-    const mergedArray = Array.from(eligibleByName.values());
-    console.log(eligibleByName);
-    console.log(mergedArray);
+    const extendedEligibleWithPartialyApplicants = Array.from(extendedEligibleWithPartialyApplicantByName.values());
 
-    const filteredArr = mergedArray.filter(
-      (object) =>
-        object.hasOwnProperty("ประเภทการเข้า") &&
-        object.hasOwnProperty("ชื่อสถานศึกษา")
-    );
-    const templatedArray = filteredArr.map((student) => {
-      return (
-      {
-        รหัสนักศึกษา: student["รหัสนักศึกษา"],
-        "ชื่อ-สกุล": student["ชื่อ - สกุล"],
-        ประเภทการเข้า: student["ประเภทการเข้า"],
-        โรงเรียน: student["ชื่อสถานศึกษา"],
-        จังหวัด: student["จังหวัดสถานศึกษา"],
-        GPAX: student["GPAX"],
-        "GPA Math": student["รายการคะแนนกลุ่มสาระวิชา_คณิตศาสตร์"],
-        "GPA Science": student["รายการคะแนนกลุ่มสาระวิชา_วิทยาศาสตร์"],
-        "GPA English": student["รายการคะแนนกลุ่มสาระวิชา_ภาษาต่างประเทศ"],
-        "GPAX 1": student[`1/${Number(student["year"])}`] || "",
-        "GPAX 2": student[`2/${Number(student["year"])}`] || "",
-        "GPAX 3": student[`1/${Number(student["year"]) + 1}`] || "",
-        "GPAX 4": student[`2/${Number(student["year"]) + 1}`] || "",
-        "GPAX 5": student[`1/${Number(student["year"]) + 2}`] || "",
-        "GPAX 6": student[`2/${Number(student["year"]) + 2}`] || "",
-        "GPAX 7": student[`1/${Number(student["year"]) + 3}`] || "",
-        "GPAX 8": student[`2/${Number(student["year"]) + 3}`] || "",
-        จำนวนหน่วยกิตรวม: 0,
-        หมายเหตุ: student["หมายเหตุ"],
-      }
-    )
-    });
-    console.log(filteredArr);
-    console.log(templatedArray);
-    const workSheet = xlsx.utils.json_to_sheet(templatedArray);
+    const filteredArr = extendedEligibleWithPartialyApplicants.filter(isExtendedEligibleWithApplicant);
 
-    xlsx.utils.book_append_sheet(workBook, workSheet, "Students");
+    const regStudents = filteredArr.filter((student) => {
+      return student["สาขาวิชาที่สมัคร"] === "วิศวกรรมคอมพิวเตอร์ - วศ.บ. 4 ปี"
+    })
+    const interStudents = filteredArr.filter((student) => {
+      return student["สาขาวิชาที่สมัคร"] === "วิศวกรรมคอมพิวเตอร์ (หลักสูตรนานาชาติ) - วศ.บ. 4 ปี"
+    })
+    
+    const reg = regStudents.map(extendedEligibleWithApplicantToPyaoResult);
+    const inter = interStudents.map(extendedEligibleWithApplicantToPyaoResult);
+
+    const regWorksheet = xlsx.utils.json_to_sheet(reg);
+    const interWorksheet = xlsx.utils.json_to_sheet(inter);
+    
+    xlsx.utils.book_append_sheet(workBook, regWorksheet, "reg");
+    xlsx.utils.book_append_sheet(workBook, interWorksheet, "inter");
+
     xlsx.writeFile(workBook, "output.xlsx");
   };
 
@@ -176,7 +191,7 @@ const MultipleFileUploader = () => {
   }, [applicants, eligibles]);
 
   return (
-    <div className="container relative mx-auto py-5 space-y-3 bg-white/20 shadow-xl rounded-md px-5">
+    <div className="container relative mx-auto py-5 space-y-3 bg-white/20 shadow-xl rounded-xl px-5">
       {/* eslint-disable-next-line jsx-a11y/alt-text */}
       {/* <img src="https://media.tenor.com/Jojpr9QgMLoAAAAd/maxwell-maxwell-spin.gif" /> */}
       <p className="text-xl font-bold">
@@ -218,43 +233,46 @@ const MultipleFileUploader = () => {
           )}
         </Dropzone>
       </div>
-      <div className="p-10 font-mono">
+      <div className="py-3 px-10 font-mono bg-white/20 ">
         {"Uplaoded applicant files"}
-        <ul>
+        <ul className="h-32 overflow-y-auto  scrollbar-thumb-blue-900 scrollbar scrollbar-track-white/10 ">
           {applicantFiles.map((file, i) => (
             <li key={i}>
-              [{file.type}] {file.name} - {file.size} bytes
+              [{(file as any).path}] [{file.type}] {file.name} - {file.size} bytes
             </li>
           ))}
         </ul>
       </div>
-      <div className="p-10 font-mono">
+      <div className="py-3 px-10 font-mono bg-white/20">
         {"Uploaded eligible files"}
-        <ul>
+        <ul className="h-32 overflow-y-auto  scrollbar-thumb-blue-900 scrollbar scrollbar-track-white/10">
           {eligibleFiles.map((file, i) => (
             <li key={i}>
-              [{file.type}] {file.name} - {file.size} bytes
+              [{(file as any).path}] [{file.type}] {file.name} - {file.size} bytes
             </li>
           ))}
         </ul>
       </div>
-      <button
+     <div className="flex justify-between">
+     <div>
+     <button
         className="bg-blue-500 hover:bg-blue-700 text-white  font-mono font-bold py-2 px-4 rounded hover:animate-bounce"
         type="button"
         onClick={exportFile}
       >
-        {"Cut the blue wire"}
+        {"Cut the red wire"}
       </button>
       <button
         className=" hover:bg-red-800 bg-red-500 text-white font-mono font-bold py-2 px-4 rounded hover:animate-bounce"
         type="button"
         onClick={exportFile}
       >
-        {"Cut the red wire"}
+        {"Cut the blue wire"}
       </button>
+     </div>
 
       <button
-        className="bg-white hover:bg-red-700 text-black font-mono font-bold py-2 px-4 rounded hover:animate-bounce"
+        className="bg-white hover:bg-gray-400 text-black font-mono font-bold py-2 px-4 rounded hover:animate-bounce"
         type="button"
         onClick={() => {
           setApplicantFiles([]);
@@ -264,9 +282,10 @@ const MultipleFileUploader = () => {
           setLogs(0);
         }}
       >
-        {"Detonate (clear all uploaded files)"}
+        {"☕ Detonate (clear all uploaded files)"}
       </button>
 
+     </div>
       {logs > 0 && <div className="p-10 font-mono">
         {`Logs >>> ${logs} (check at the console for more information)`}
       </div>}
